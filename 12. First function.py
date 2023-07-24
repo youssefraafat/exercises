f4dadf0ab1f17cb0594741dae9feacdbd6701f87
youssefraafat <yosefraft19@gmail.com>
2023-07-24 20:07:27 +0300

    Create 12. First function.py

---
+++ 12. First function.py
@@ -0,0 +1,11 @@
+# Type your code here:
+def circle_perimeter(radius):
+    import math
+    return 2*math.pi*radius
+
+# You don't need to modify the follwoing part
+# it allows you to test your code before submitting it:
+
+if __name__ == "__main__":
+    print(circle_perimeter(1))
+    
